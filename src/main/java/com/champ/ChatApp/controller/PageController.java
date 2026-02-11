8834012aad440a713924eb346daaf1a16e3410b7
Cyber-champ <132355373+Cyber-champ@users.noreply.github.com>
2026-02-11 22:48:16 +0530

    Add files via upload

---
+++ src/main/java/com/champ/ChatApp/controller/PageController.java
@@ -0,0 +1,18 @@
+package com.champ.ChatApp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PageController {
+
+    @GetMapping("/")
+    public String home() {
+        return "index";
+    }
+
+    @GetMapping("/chat")
+    public String chat() {
+        return "chat";
+    }
+}
